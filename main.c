3bd3d66e39a93d1a5b254fd326313ccc77ba8b24
SigCatta <106386991+SigCatta@users.noreply.github.com>
2022-06-10 14:52:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-char new[] = "nuova_partita";
-char filter[] = "stampa_filtrate";
-char begin[] = "inserisci_inizio";
-char end[] = "inserisci_fine";
+#define DIC_SIZE 64
 
 int k, attempts, gameon = 1;
-char *r, *p, *discovered;
-int dictionary[54] = {};
-
+char *r, *p, *discovered, *buffer;
+int dictionary[DIC_SIZE] = {};
+char **not_present;
+int w;
 /*          DICTIONARY
  *  lower case ASCII -> 97-122           ---> 0-25
  *  caps lock ASCII -> 65- 90            ---> 26-51
  *  minus -> 45 . underscore -> 126      ---> 52-53
- *  #symbols -> 54
+ *  numbers -> 48 - 57                   ---> 54 -63
+ *  #symbols -> 64
  */
 
 typedef struct {
     char *key;
-    struct node *father;
     struct node *left;
     struct node *right;
     char v;
-}node;
+} node;
 
 node *root;
 
-char d_convert(int n){
-    if (n <=25) return 'a' + n;
+int exists(node *n) {
+    if (n->key != NULL) {
+        int value = memcmp(p, n->key, k);
+        if (value == 0) return 0;
+        if (value < 0) return exists((node *) n->left);
+        return exists((node *) n->right);
+    }
+    return 1;
+}
+
+char d_convert(int n) {
+    if (n <= 25) return 'a' + n;
+    if (n >= 54) return '0' + (n - 48);
     if (n == 52) return '-';
     if (n == 53) return '_';
     return 'A' + (n - 26);
 }
 
-void count_letters(node *n){
+void inorder_tree_walk(node *n) {
+    if (n->key != NULL) {
+        inorder_tree_walk((node *) n->left);
+        if (n->v == 'y') printf("%s\n", n->key);
+        inorder_tree_walk((node *) n->right);
+    }
+}
+
+void count_letters(node *n) {
     int count = 0;
-    for (int i = 0; i < 54; i++) {
-        if (dictionary[i] > 0){
+    for (int i = 0; i < DIC_SIZE; i++) {
+        if (dictionary[i] > 0) {
             for (int j = 0; j < k; j++) {
                 if (n->key[j] == d_convert(i)) count++;
             }
-            if (count != dictionary[i]){
+            if (count != dictionary[i]) {
                 n->v = 'n';
                 return;
             }
@@ -51,25 +69,25 @@ void count_letters(node *n){
     }
 }
 
-void update_v(node *n){
-    if (n != NULL){
-        while (n->v == 'y') {
-            for (int i = 0; i < k; i++) {
-                if (n->key[i] != '#') {
-                    if (n->key[i] != discovered[i]) {
-                        n->v = 'n';
-                    }
+void update_v(node *n) {
+    if (n->key != NULL && n->v == 'y') {
+        for (int i = 0; i < k; i++) {
+            if (discovered[i] != '#') {
+                if (n->key[i] != discovered[i]) {
+                    n->v = 'n';
+                    break;
                 }
             }
-            count_letters(n);
         }
+        if (n->v == 'y') count_letters(n);
         update_v((node *) n->left);
         update_v((node *) n->right);
     }
+    return;
 }
 
-void valid_all(node *n){
-    if (n != NULL){
+void valid_all(node *n) {
+    if (n->key != NULL) {
         valid_all((node *) n->left);
         n->v = 'y';
         valid_all((node *) n->right);
@@ -77,70 +95,55 @@ void valid_all(node *n){
     return;
 }
 
-void inorder_tree_walk(node *n){
-    if (n != NULL){
-        inorder_tree_walk((node *) n->left);
-        if (n->v == 'y') printf("%s\n", n->key);
-        inorder_tree_walk((node *) n->right);
-    }
-    return;
-}
-
-void add(char *word, node *n, node *father){
-    if (n->key == NULL) {
-        n = malloc(sizeof(node));
-        n->key = word;
-        n->left = NULL;
-        n->right = NULL;
-        n->father = (struct node *) father;
-        n->v = 'y';
-        return;
-    }
-    for(int i=0; i<k; i++){
-        if (word[i] < n->key[i]) return add(word, (node *) n->left, n);
-        if (word[i] > n->key[i]) return add(word, (node *) n->right, n);
-    }
-    printf("ERRORE");
-    return;
-}
-
-void reset(){
+void reset() {
     valid_all(root);
-    for (int i = 0; i < k; i++) discovered[i] = '#';
-    for (int i = 0; i < 54; i++) dictionary[i] = 0;
+    memset(discovered, '#', k);
+    memset(dictionary, 0, sizeof(dictionary));
 }
 
-void command(){
-    char buffer[k];
-    scanf("%s", buffer);
-    if (strcmp(buffer, new) == 0) {
+void *initialize(node *n) {
+    n = malloc(sizeof(node));
+    n->key = NULL;
+    n->v = 'n';
+    return n;
+}
+
+void *add(node *n) {
+    if (n->key == NULL) {
+        n->key = malloc(k);
+        memcpy(n->key, buffer, k + 1);
+        n->left = initialize((node *) n->left);
+        n->right = initialize((node *) n->right);
+        n->v = 'y';
+        return 0;
+    }
+    if (memcmp(buffer, n->key, k) < 0) return add((node *) n->left);
+    if (memcmp(buffer, n->key, k) > 0) return add((node *) n->right);
+    return 0;
+}
+
+void command(char *string) {
+    if (string[1] == 'n') {
         reset();
-        for (int j = 0; j < k; j++) scanf("%c", r[j]);
-        scanf("%d", &attempts);
+        fflush(stdin);
+        fgets(r, k + 1, stdin);
+        w = scanf("%d", &attempts);
         gameon = 0;
         return;
     }
-    if (strcmp(buffer, begin) == 0) {
-        while(1) {
-            for (int j = 0; j < k; j++){
-                scanf("%c", buffer[j]);
-                if (buffer[0] == '+') {
-                    scanf("%s", buffer);
-                    if (strcmp(buffer, end) == 0) {
-                        gameon =0;
-                        return;
-                    }
-                    else{
-                        printf("ERRORE");
-                        return;
-                    }
-                }
+    if (string[1] == 'i') {
+        while (1) {
+            fgets(buffer, k + 1, stdin);
+            if (buffer[0] == '+') {
+                if (buffer[1] == 'i') {
+                    gameon = 0;
+                    return;
+                } else command(buffer);
             }
-            add(buffer, root, NULL);
+            add(root);
         }
     }
-    if (strcmp(buffer, filter) == 0) {
-        gameon = 0;
+    if (string[1] == 's') {
         update_v(root);
         inorder_tree_walk(root);
         return;
@@ -149,126 +152,132 @@ void command(){
 }
 
 void pregame() {
-    char buffer[k];
     while (1) {
-        if (gameon == -1) break;
-        for (int i = 0; i < k; i++) {
-            scanf("%c", buffer[i]);
-            if (buffer[0] == '+') {
-                command();
-                return;
-            }
+        fflush(stdin);
+        fgets(buffer, k + 1, stdin);
+        if (buffer[0] == '+') {
+            command(buffer);
+            return;
         }
-        add(buffer, root, NULL);
+        add(root);
     }
-    return;
 }
 
-int present (char *word){
-    for (int i = 0; i<k; i++){
-        if (r[i] == word[i]) return 0;
-    }
-    return 1;
-}
-
-void d_update(char c, char bool, int count){
+void d_update(char c, char bool, int count) {
     if (bool == 'n') {
+        if (islower(c) != 0) {
+            dictionary[c - 97]--;
+            return;
+        }
+        if (isupper(c) != 0) {
+            dictionary[c - 39]--;
+            return;
+        }
+        if (isalnum(c) != 0) {
+            dictionary[c + 6]--;
+            return;
+        }
         if (c == 126) {
             dictionary[53]--;
             return;
         }
-        if (c == 45) {
-            dictionary[52]--;
-            return;
-        }
-        if (c >= 97) {
-            dictionary[c - 97]--;
-            return;
-        }
-        if (c <= 65) {
-            dictionary[c - 39]--;
-            return;
-        }
+        dictionary[52]--;
+        return;
+    }
+    if (islower(c) != 0) {
+        dictionary[c - 97] = count;
+        return;
+    }
+    if (isupper(c) != 0) {
+        dictionary[c - 39] = count;
+        return;
+
+    }
+    if (isalnum(c) != 0) {
+        dictionary[c + 6] = count;
+        return;
     }
     if (c == 126) {
         dictionary[53] = count;
         return;
     }
-    if (c == 45) {
-        dictionary[52] = count;
-        return;
-    }
-    if (c >= 97) {
-        dictionary[c - 97] = count;
-        return;
-    }
-    if (c <= 65) {
-        dictionary[c - 39] = count;
-        return;
-    }
+    dictionary[52] = count;
+    return;
 }
 
-void game(){
-    for (int i = 0; i<k; i++){
+void game() {
+    if (exists(root) == 1) {
+        printf("not_exists\n");
+        return;
+    }
+    for (int i = 0; i < k; i++) {
         if (r[i] == p[i]) {
             printf("+");
             discovered[i] = p[i];
-        }
-        else if (present(p) == 1) {
+        } else if (memchr(r, p[i], k) == NULL) {
             d_update(p[i], 'n', 0);
             printf("/");
-        }
-        else {
-            int recA = 0, recB =0, recP = 0;
-            for (int j = 0; j <k; j++) {
+            strcat(not_present[i], &p[i]);
+        } else {
+            int recA = 0, recB = 0, recP = 0;
+            for (int j = 0; j < k; j++) {
                 if (r[i] == r[j]) recA++;
                 if (p[i] == p[j]) {
                     recB++;
-                    if (j<i) recP++;
+                    if (j < i) recP++;
                 }
             }
-            if (recA >= recB || recB > i || recB - recP > 0 ) {
+            if (recA >= recB || recB > i || recB - recP > 0) {
                 printf("|");
-            }
-            else {
+                strcat(not_present[i], &p[i]);
+            } else {
                 d_update(p[i], 'y', recA);
                 printf("/");
+                strcat(not_present[i], &p[i]);
             }
         }
     }
+    printf(", %d\n", --attempts);
+    return;
 }
 
 int main() {
-    char buffer[k];
-    scanf("%d", &k);
+    if (w == -1) w = 0;
+    w = scanf("%d", &k);
+    r = malloc(k);
+    p = malloc(k);
+    not_present = calloc(k, sizeof(char *));
+    for (int i = 0; i < k; i++) not_present[i] = malloc(DIC_SIZE - 1);
+    discovered = malloc(k);
+    memset(discovered, '#', k);
     root = malloc(sizeof(node));
-    root->father = NULL;
     root->v = 'y';
-    for (int i = 0; i < k; i++) scanf("%c", buffer);
-    root->key = buffer;
+    fflush(stdin);
+    root->key = malloc(k);
+    fgets(root->key, k + 1, stdin);
+    root->left = initialize((node *) root->left);
+    root->right = initialize((node *) root->right);
+    buffer = malloc(k);
     pregame();
-    p = malloc(sizeof(char)*k);
-    discovered = malloc(sizeof(char)*k);
-    for (int i = 0; i < k; i++) {
-        discovered[i] = '#';
-    }
-    r = malloc(sizeof(char)*k);
     while (1) {
-        if (gameon == -1) break;
+        if (gameon == -2) break;
         if (attempts == 0) {
             printf("ko");
             pregame();
         }
-        for (int i = 0; i < k; i++) scanf("%c", p[i]);
-        if (p[0] == '+') command();
-        if (strcmp(r,p) == 0) {
+        gameon = 0;
+        fflush(stdin);
+        fgets(p, k + 1, stdin);
+        if (p[0] == '+') {
+            command(p);
+            gameon = 2;
+        }
+        if (memcmp(r, p, k) == 0) {
             gameon = 1;
             printf("ok");
         }
-        if (gameon == 0){
+        if (gameon == 0) {
             game();
-            printf("\n");
-            attempts--;
         }
         if (gameon == 1) pregame();
     }
