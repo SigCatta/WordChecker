9eb19bdf942797a68be63f403fb6aa00199001b2
SigCatta <106386991+SigCatta@users.noreply.github.com>
2022-08-19 14:52:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -168,6 +168,31 @@ void update_v(node *n) {
     }
 }
 
+void first_update(node *n) {
+    if (n != NULL) {
+        first_update((node *) n->left);
+        first_update((node *) n->right);
+        n->v = 'y';
+        for (int i = 0; i < k; i++) {
+            if (discovered[i] != '#') {
+                if (n->key[i] != discovered[i]) {
+                    n->v = 'n';
+                    return;
+                }
+            }
+            if (fastmemchr(not_present[i], n->key[i]) != NULL) {
+                n->v = 'n';
+                return;
+            }
+        }
+        if (count_letters(n->key) == 'n') {
+            n->v = 'n';
+            return;
+        }
+        valid++;
+    }
+}
+
 node *new_node_ins() {
     node *n = malloc(sizeof(node));
     n->key = buffer;
@@ -182,6 +207,7 @@ node *new_node_ins() {
     }
     if (count_letters(n->key) == 'n') return n;
     n->v = 'y';
+    if (valid == 0) return n;
     valid++;
     return n;
 }
@@ -194,21 +220,16 @@ void add_ins(node **tree) {
     (*tree) = new_node_ins();
 }
 
-void valid_all(node *n) {
+void print_all(node *n) {
     if (n != NULL) {
-        valid_all((node *) n->left);
-        n->v = 'y';
-        valid_all((node *) n->right);
+        print_all((node *) n->left);
+        puts(n->key);
+        print_all((node *) n->right);
     }
 }
 
 void command(char *string) {
     if (string[1] == 'n') {
-        for (int i = 0; i < DIC_SIZE; i++) {
-            for (int j = 0; j < DIC_SIZE; j++) {
-                valid_all(hash_table[i][j]);
-            }
-        }
         memset(discovered, '#', k);
         memset(dictionary, 0, DIC_SIZE * sizeof(int));
         for (int i = 0; i < k; i++) memset(not_present[i], '#', k);
@@ -233,7 +254,13 @@ void command(char *string) {
             add_ins(&hash_table[hash(buffer[0])][hash(buffer[1])]);
         }
     }
-    if (valid != 1)print_table();
+    if (valid == 0) {
+        for (int i = 0; i < DIC_SIZE; i++) {
+            for (int j = 0; j < DIC_SIZE; j++) {
+                print_all(hash_table[i][j]);
+            }
+        }
+    }else if (valid > 1)print_table();
     else puts(r);
 }
 
@@ -308,7 +335,15 @@ void game() {
             continue;
         }
     }
-    if (valid != 1) {
+    if (valid == 0) {
+        for (int i = 0; i < DIC_SIZE; i++) {
+            if (dictionary[i] == -1) continue;
+            for (int j = 0; j < DIC_SIZE; j++) {
+                if (dictionary[j] == -1) continue;
+                first_update(hash_table[i][j]);
+            }
+        }
+    } else if (valid > 1) {
         valid = 0;
         if (discovered[0] != '#') {
             if (discovered[1] != '#') {
